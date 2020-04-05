2c82b3dbe5663c9bbdf3533bcfe2618bdb3c570f
dep01 <diazerlanggaputra@ymail.com>
2020-04-05 20:52:44 +0700

    Dashboard

--- application/views/App/Project/Project_list.php
+++ application/views/App/Project/Project_list.php
@@ -7,9 +7,34 @@
         ['title'=>'On Progress',
         'count'=>20],
     );
+    
  ?>
 <div class="row">
+
   <?php foreach($data['list'] as $list): ?>
     <?php include('inc/Project_detail.php'); ?>
   <?php endforeach;?>
+</div>
+<?php 
+    $data['list']=array(
+        ['title'=>'Perpustakaan',
+        'author'=>'Dani andri',
+        'start'=>'28,December 2019',
+        'end'=>'28,February 2020'],
+        ['title'=>'Project Management',
+        'author'=>'Diaz erlangga',
+        'start'=>'28,February 2020',
+        'end'=>'28,Mei 2020'],
+        ['title'=>'Cloud Management',
+        'author'=>'Rio baskara',
+        'start'=>'28,March 2020',
+        'end'=>'28,Mei 2020']
+    );
+    
+ ?>
+<div class="column">
+<a href="#" class="btn btn-primary">Create new project</a>
+    <?php foreach($data['list'] as $list): ?>
+      <?php include('inc/Project_card.php'); ?>
+    <?php endforeach;?>
 </div>
